364cafb1bb92b3fd33a492a7b373bcebc0136a8f
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-02-16 21:12:43 -0330

    ts practice

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,18 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+class Component<T> {
+	constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
-    console.log(this.props.title);
-  }
+interface Props {
+	title: string;
+}
+
+class Page extends Component<Props> {
+	pageInfo() {
+		console.log(this.props.title);
+	}
 }
 
 export {};
